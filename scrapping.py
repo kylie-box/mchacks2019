b7cc891c918889ed4da71eda93af0c62449ea7ce
Yihe Zhang <37360880+YiheZ@users.noreply.github.com>
2019-02-02 18:48:43 -0500

    data_file

--- scrapping.py
+++ scrapping.py
@@ -21,10 +21,13 @@ APIurl = 'https://sandbox86.tactiorpm7000.com/tactio-clinician-api/1.1.4'
 # print(json_data)
 
 
-patient_info_raw = requests.get(APIurl+'/patient',headers={'Authorization':'token {}'.format()}) # put token here
+patient_info_raw = requests.get(APIurl+'/patient',headers={'Authorization':'token {}'.format('1c6069aae80f38638ffb9c53dec6137ac99b5f4b')}) # put token here
 patient_info_json = json.loads(patient_info_raw.text)
 # print(patient_info_json)
 
-patient_observation_raw = requests.get(APIurl+'/Observation',headers={'Authorization':'token {}'.format()}) # put token here
+patient_observation_raw = requests.get(APIurl+'/Observation',headers={'Authorization':'token {}'.format('1c6069aae80f38638ffb9c53dec6137ac99b5f4b')}) # put token here
 
-print(patient_observation_raw.text)
+#print(patient_observation_raw.text)
+
+with open("/data_file.json", "r") as write_files:
+    json.dump(patient_observation_raw.json(), write_files)
